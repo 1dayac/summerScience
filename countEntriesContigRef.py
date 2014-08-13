0fdaaa08f33e7803f046c4ba646785b6f6530bcd
Dmitrii Meleshko <Dmitrii@MeleshkoDima.(none)>
2014-08-13 15:49:11 +0400

    Now script is working ok

--- countEntriesContigRef.py
+++ countEntriesContigRef.py
@@ -1,11 +1,37 @@
 __author__ = 'Dmitrii'
-
 from Bio import SeqIO
 import sys
 import os
+import getopt
+#import time
 
-os.system("makeblastdb -in " + sys.argv[1] + " -dbtype nucl");
-os.system("blastn -db " + sys.argv[2] + " -query " + sys.argv[1] + " -outfmt 6 -dust yes -word_size 50 -evalue 10 -out results.txt");
+def usage():
+    print 'usage: countEntriesContigRef.py contigs.fa reference.fa contigs.astat'
+    print 'Find set of contigs with likely unique copy number'
+    print 'Options:'
+    print '    -align-rate INT (0-100)         minimum rate of good aligned bp needed to keep alignment (default - 95)'
+    print '    -length-diff INT                maximum difference of length between contig and part of reference to keep alignment (default - 10)'
+
+if len(sys.argv) <= 2:
+    usage()
+    exit()
+
+align_rate = 0.95
+length_diff = 10
+
+
+opts, args = getopt.getopt(sys.argv[1:], "", ["align-rate", "length-diff"])
+
+for o, a in opts:
+   if o == "align-rate":
+       align_rate = float(a)
+   elif o == "length-diff":
+       length_diff = int(a)
+
+os.system("makeblastdb -in " + sys.argv[2] + " -dbtype nucl")
+#time.sleep(25)
+os.system("blastn -db " + sys.argv[2] + " -query " + sys.argv[1] + " -outfmt 6 -dust yes -word_size 50 -evalue 10 -out results.txt")
+#time.sleep(25)
 
 
 query_fasta = SeqIO.to_dict(SeqIO.parse(open(sys.argv[1], "rU"), "fasta"))
@@ -18,7 +44,7 @@ with open("results.txt", 'r') as blastFile:
     for line in blastFile:
         split_line = line.split('\t')
         contig_name = split_line[0]
-        if len(query_fasta[contig_name]) * 0.96 <= int(split_line[3]):
+        if len(query_fasta[contig_name]) - length_diff <= int(split_line[3]):
             if contig_name in result_dict:
                 result_dict[contig_name] += 1
             else:
@@ -33,13 +59,20 @@ not_ok_multiple = 0
 
 numberOfAl = {}
 
+lengthes_unique = set()
+lengthes_unique_wrong = set()
+lengthes_multiple = set()
+lengthes_multiple_wrong = set()
+
 with open(sys.argv[3], 'r') as astatFile:
     for line in astatFile:
+#        print(line)
         line_splited = line.split()
-        if float(line_splited[5]) > 20.0:
+        if float(line_splited[5]) > 600.0:
             if line_splited[0] not in result_dict or result_dict[line_splited[0]] <= 1:
                 print(line_splited[0] + " - ok - unique or missing")
                 ok_unique += 1
+                lengthes_unique.add(line_splited[1])
             else:
                 print(line_splited[0] + " - not ok - unique A-stat but multiple alignments")
                 not_ok_unique += 1
@@ -47,18 +80,27 @@ with open(sys.argv[3], 'r') as astatFile:
                     numberOfAl[result_dict[line_splited[0]]] += 1
                 else:
                     numberOfAl[result_dict[line_splited[0]]] = 1
+                lengthes_unique_wrong.add(line_splited[1])
+
         else:
             if line_splited[0] not in result_dict or result_dict[line_splited[0]] <= 1:
                 print(line_splited[0] + " - not ok - multiple A-stat and unique or missing alignments")
                 not_ok_multiple += 1
+                lengthes_multiple_wrong.add(line_splited[1])
             else:
                 print(line_splited[0] + " - ok - multiple A-stat and multiple alignments")
                 ok_multiple += 1
+                lengthes_multiple.add(line_splited[1])
 
 print("ok - unique or missing - " + str(ok_unique))
+print(lengthes_unique)
 print("not ok - unique A-stat but multiple alignments - " + str(not_ok_unique))
+print(lengthes_unique_wrong)
+
 print("not ok - multiple A-stat and unique or missing alignments - " + str(not_ok_multiple))
+print(lengthes_multiple_wrong)
 print("ok - multiple A-stat and multiple alignments - " + str(ok_multiple))
+print(lengthes_multiple)
 
 print(numberOfAl)
 
